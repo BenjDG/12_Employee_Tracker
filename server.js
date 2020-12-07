dda87a6368d6b100309ed44d41ed70976588d4e6
Ben Galloway <bdgalloway85@gmail.com>
2020-12-06 19:39:46 -0600

    inquirer build start

--- server.js
+++ server.js
@@ -10,8 +10,47 @@ const connection = mysql.createConnection({
     database: "employee_tracker_db"
 });
 
-connection.connect(function(err) {
+connection.connect(function (err) {
     if (err) throw err;
     console.log("connected as id " + connection.threadId);
     start();
 });
+
+function start() {
+    inquirer
+        .prompt({
+            name: "action",
+            type: "list",
+            message: "What would you like to do?",
+            choices: [
+                "View All Employees",
+                "View All Employees by Department",
+                "View All Employees by Manager",
+                "View All Departments",
+                "View All Roles"
+            ]
+        })
+        .then(function (answer) {
+            switch (answer.action) {
+                case "View All Employees":
+                    viewAllEmployees();
+                    break;
+
+                case "View All Employees by Department":
+                
+                break;
+
+                case "View All Employees by Manager":
+                
+                break;
+
+                case "View All Departments":
+                
+                break;
+
+                case "View All Roles":
+                
+                break;
+            }
+        })
+}
